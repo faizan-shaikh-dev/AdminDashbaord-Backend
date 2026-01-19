c15a163838638a1f0c26f6a4c0b05a59ef4b8510
faizan-shaikh-dev <faizangulab4276@gmail.com>
2026-01-19 10:29:28 +0530

    chnage url

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.use(cors({ origin: "https://admindashbaord-backend.onrender.com/", credentials: true }));
+app.use(cors({ origin: "https://dashboard-app-production-94b6.up.railway.app", credentials: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
